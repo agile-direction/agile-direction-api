5a642613c5bedd4ca595eed63c3e58215029c314
Steve Weber <steve@copyright1984.com>
2015-09-20 15:02:26 -0500

    [feature] collapse completed deliverables

---
+++ app/assets/javascripts/expandable.js
@@ -0,0 +1,19 @@
+(function() {
+  var handleClick = function(event) {
+    event.preventDefault();
+    $(event.target)
+      .closest('.expandable')
+      .toggleClass('collapsed');
+  }
+
+  var ready = function() {
+    $('.expandable').each(function(index, ui) {
+      var expandableElement = $(ui);
+      expandableElement.find('.expand').eq(0).on('click', handleClick);
+      expandableElement.find('.collapse').eq(0).on('click', handleClick);
+    });
+  }
+
+  $(document).ready(ready);
+  $(document).on('page:load', ready);
+})();
