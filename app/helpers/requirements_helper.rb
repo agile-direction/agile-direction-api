1ffb3ad77b5fa2aba80055e970f760fc33254e1e
Steve Weber <sweber@groupon.com>
2015-10-06 19:29:01 -0500

    [links] custom implementation, it's going to be a pain to escape only the links

--- app/helpers/requirements_helper.rb
+++ app/helpers/requirements_helper.rb
@@ -1,2 +1,38 @@
 module RequirementsHelper
+  ALLOWED_TLDS = %w(
+    com
+    info
+    net
+    org
+    gov
+    au
+    br
+    cn
+    de
+    es
+    fr
+    in
+    ir
+    it
+    jp
+    nl
+    pl
+    ru
+    uk
+  )
+  DEFAULT_PROTOCOL = "http://"
+  PROTOCOL_MATCHER = /:\/\//
+  HTTP_PROTOCOL = "http"
+  ALLOWED_TLDS_REGEX = "(#{ALLOWED_TLDS.join("|")})"
+  ALLOWED_DOMAIN_CHARACTERS = /([\w\-]+\.)+/
+  ALLOWED_DOMAINS = %r{#{ALLOWED_DOMAIN_CHARACTERS}#{ALLOWED_TLDS_REGEX}}
+
+  def inject_links(string)
+    result = string.gsub(%r{(\w+?:\/\/)?(#{ALLOWED_DOMAINS})(\/.*)?}) do |url|
+      url = "#{HTTP_PROTOCOL}://#{url}" unless url.match(PROTOCOL_MATCHER)
+      return url unless url.match(HTTP_PROTOCOL)
+      link_text = url.match(ALLOWED_DOMAINS)
+      content_tag(:a, link_text, { href: "#{url}" }).html_safe
+    end
+  end
 end
--- spec/helpers/requirements_helper_spec.rb
+++ spec/helpers/requirements_helper_spec.rb
@@ -10,5 +10,61 @@ require 'rails_helper'
 #     end
 #   end
 # end
-RSpec.describe RequirementsHelper, type: :helper do
+RSpec.describe(RequirementsHelper, { type: :helper }) do
+  describe "#inject_links" do
+    describe "finding links" do
+      it "returns links without http" do
+        expect(helper.inject_links("foo.com")).to match(%r{^<a href=\"http://foo.com\"})
+      end
+
+      it "returns links with http" do
+        expect(helper.inject_links("http://foo.com")).to match(%r{^<a href=\"http://foo.com\"})
+      end
+
+      it "returns links with https" do
+        expect(helper.inject_links("https://foo.com")).to match(%r{^<a href=\"https://foo.com\"})
+      end
+
+      it "does not link other protocols" do
+        expect(helper.inject_links("ftp://foo.com")).to match(%r{^ftp://foo.com})
+      end
+
+      it "returns links for popular domains" do
+        %w(com net ru org de jp uk br pl in it fr info au nl cn ir es gov).each do |tld|
+          string  = "foo.#{tld}"
+          expect(helper.inject_links(string)).to match(%r{^<a href=\"http://foo.#{tld}\"})
+        end
+      end
+
+      it "does not return links for other domains" do
+        expect(helper.inject_links("foo.xx")).to match(%r{^foo.xx$})
+      end
+
+      it "returns links with subdomains" do
+        expect(helper.inject_links("www.deep.foo.com")).to match(%r{^<a href=\"http://www.deep.foo.com\"})
+      end
+
+      it "returns multiple links" do
+        string = "http://foo.com and bar.com"
+        expect(helper.inject_links(string)).to match(%r{^<a href=\"http://foo.com\"})
+        expect(helper.inject_links(string)).to match(%r{and <a href=\"http://bar.com\"})
+      end
+
+      it "returns link with path" do
+        expect(helper.inject_links("foo.com/bar?baz")).to match(%r{^<a href=\"http://foo.com/bar\?baz\"})
+      end
+
+      it "matches all valid domain names" do
+        expect(helper.inject_links("f-Oo.com")).to match(%r{^<a href=\"http://f-Oo.com\"})
+      end
+    end
+
+    it "returns links with domain as its content" do
+      expect(helper.inject_links("https://foo.com/bar")).to match(%r{^<a href=\"https://foo.com\/bar\">foo.com})
+    end
+
+    it "returns links within content" do
+      expect(helper.inject_links("find foo here foo.com")).to match(%r{find foo here <a href=\"http://foo.com\"})
+    end
+  end
 end
